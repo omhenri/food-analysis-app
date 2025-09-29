0c639afda3e4724f6ab1680ae0625ad2e2165f8f
Henry <hichlaw@gmail.com>
2025-09-29 11:52:21 +0800

    update the splash screen

--- FoodAnalysisApp/android/app/src/main/java/com/foodanalysisapp/MainActivity.kt
+++ FoodAnalysisApp/android/app/src/main/java/com/foodanalysisapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.foodanalysisapp
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    // Switch from SplashTheme to AppTheme after splash screen
+    setTheme(R.style.AppTheme)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
